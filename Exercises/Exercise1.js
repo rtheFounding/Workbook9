e27a66670e780aedc63f2996769e0860339723ec
Randy Rivera <rivera.na2015@gmail.com>
2022-11-28 11:07:14 -0500

    Added a method named promote() with parameters, with new values for job title and pay

--- Exercises/Exercise1.js
+++ Exercises/Exercise1.js
@@ -10,10 +10,19 @@ class Employee {
   getFullName() {
     return this.firstName + " " + this.lastName;
   }
+
+  // in this new method, we assigned the parameter values to the appropriate properties of the object
+  promote(newJobTitle, newPayRate) {
+    this.jobTitle = newJobTitle;
+    this.payRate = newPayRate;
+  }
 }
 
 let employee1 = new Employee(1, "Ian", "Auston", "Graphic Artist", 42.5);
-let employee2 = new Employee(2, "Ezio", "Auditore", "Protector", 100.5 );
+let employee2 = new Employee(2, "Ezio", "Auditore", "Protector", 100.5);
+
+employee1.promote("Sr. Graphic Artist", 46.75);
+employee2.promote("Sr. Protector", 200.5);
 
 //access a property and print it in a template string
 console.log(`Employee ${employee1.firstName} created`);
