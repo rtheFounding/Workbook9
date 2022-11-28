7ab32b48e504e8d8298b29e41a44a37cf95418f2
Randy Rivera <rivera.na2015@gmail.com>
2022-11-28 11:47:11 -0500

    Creating new class which extends Person

--- Exercises/Exercise1-20.js
+++ Exercises/Exercise1-20.js
@@ -11,3 +11,18 @@ class Person {
 
 let randy = new Person("Randy", "Rivera");
 console.log(randy.getFullName());
+
+class Employee extends Person {
+  constructor(firstName, lastName, id, jobTitle, payRate) {
+    super(firstName, lastName);
+  }
+
+  getFullName() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+let employee1 = new Employee("Eren", "Yeager", 1, "Founding Titan", 100.5);
+let employee2 = new Employee("Cloud", "Strife", 2, "Swordsman", 150.5);
+console.log(`Employee ${employee1.getFullName()} created`);
+console.log(`Employee ${employee2.getFullName()} created`);
